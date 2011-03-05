0b3dc06d94dcc801a0bb9cf1cd192684a5dbb2e8
Ed MacDonald <edmacdonald555@gmail.com>
2011-03-05 01:06:36 -0500

    Cleanup

--- src/main/java/net/edmacdonald/javaPlayground/metaProgramming/util/Utils.java
+++ src/main/java/net/edmacdonald/javaPlayground/metaProgramming/util/Utils.java
@@ -2,63 +2,51 @@ package net.edmacdonald.javaPlayground.metaProgramming.util;
 
 import org.apache.commons.lang.ArrayUtils;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Utils
-{
-        public static boolean isImplementedMethodAnnotatedWith(Method method, Class annotation)
-        {
-            Class[] clazzes = getAllInterfaces(method.getDeclaringClass());
+public class Utils {
+    public static boolean isImplementedMethodAnnotatedWith(Method method, Class annotation) {
+        Class[] clazzes = getAllInterfaces(method.getDeclaringClass());
 
-            for(Class clazz : clazzes)
-            {
-                try
-                {
-                    //Throws an exception if method not found.
-                    Method m = clazz.getDeclaredMethod(method.getName(), method.getParameterTypes());
+        for(Class clazz : clazzes) {
+            try {
+                //Throws an exception if method not found.
+                Method m = clazz.getDeclaredMethod(method.getName(), method.getParameterTypes());
 
-                    if(null != m.getAnnotation(annotation))
-                    {
-                        return true;
-                    }
-                }
-                catch(Exception e)
-                {
-                    /*Do nothing*/
+                if(null != m.getAnnotation(annotation)) {
+                    return true;
                 }
             }
-
-            return false;
-        }
-
-        private static Class[] getAllInterfaces(Class clazz)
-        {
-            return getAllInterfaces(new Class[] { clazz } );
-        }
-
-        private static Class[] getAllInterfaces(Class[] classes)
-        {
-            if(0 == classes.length )
-            {
-                return classes;
-            }
-            else
-            {
-                List<Class> extendedClasses = new ArrayList<Class>();
-                for (Class clazz: classes)
-                {
-                    extendedClasses.addAll(Arrays.asList( clazz.getInterfaces() ) );
-                }
-                //Class::getInterfaces() gets only interfaces/classes implemented/extended directly by a given class.
-                //We need to walk the whole way up the tree.
-                return (Class[]) ArrayUtils.addAll( classes,
-                                                    getAllInterfaces(
-                                                            extendedClasses.toArray(new Class[extendedClasses.size()]))
-                );
+            catch(Exception e) {
+                /*Do nothing*/
             }
         }
+
+        return false;
+    }
+
+    private static Class[] getAllInterfaces(Class clazz) {
+        return getAllInterfaces(new Class[] { clazz } );
+    }
+
+    private static Class[] getAllInterfaces(Class[] classes) {
+        if(0 == classes.length ) {
+            return classes;
+        }
+        else {
+            List<Class> extendedClasses = new ArrayList<Class>();
+            for (Class clazz: classes) {
+                extendedClasses.addAll(Arrays.asList( clazz.getInterfaces() ) );
+            }
+            //Class::getInterfaces() gets only interfaces/classes implemented/extended directly by a given class.
+            //We need to walk the whole way up the tree.
+            return (Class[]) ArrayUtils.addAll( classes,
+                                                getAllInterfaces(
+                                                        extendedClasses.toArray(new Class[extendedClasses.size()]))
+            );
+        }
+    }
 }
